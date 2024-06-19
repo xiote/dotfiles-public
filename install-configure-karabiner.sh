9e9006c8cb43ff9a558b34a08a2ea7e1f985f1f0
xiote <xiote@sac7.local>
2024-06-19 11:05:11 +0900

    modified:   install-configure-karabiner.sh

--- install-configure-karabiner.sh
+++ install-configure-karabiner.sh
@@ -9,24 +9,6 @@ case "${unameOut}" in
     *)          MACHINE="UNKNOWN:${unameOut}"
 esac
 
-echo "Install brew..."
-if [[ $MACHINE == "Mac" ]]; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-    # Make sure we’re using the latest Homebrew.
-    brew update
-
-    # Upgrade any already-installed formulae.
-    brew upgrade
-
-elif [[ $MACHINE == "Linux" ]]; then
-    exit 1
-fi
-
-# Symlink all dotfiles on Mac
-##if [[ $MACHINE == "Mac" ]]; then
-##fi
-
 echo "Install karabiner..."
 brew install --cask karabiner-elements
 
