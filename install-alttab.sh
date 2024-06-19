decdc9ac379c466712be6d5da1d7a8441075dd6c
xiote <xiote@nate.com>
2024-06-19 11:33:17 +0900

    new file:   install-alfred.sh 	new file:   install-alttab.sh 	new file:   install-brew.sh 	new file:   install-notion.sh 	new file:   install-slack.sh

---
+++ install-alttab.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     MACHINE=Linux;;
+    Darwin*)    MACHINE=Mac;;
+    CYGWIN*)    MACHINE=Cygwin;;
+    MINGW*)     MACHINE=MinGw;;
+    *)          MACHINE="UNKNOWN:${unameOut}"
+esac
+
+echo "Install Alttab..."
+brew install --cask alt-tab
---
+++ install-slack.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     MACHINE=Linux;;
+    Darwin*)    MACHINE=Mac;;
+    CYGWIN*)    MACHINE=Cygwin;;
+    MINGW*)     MACHINE=MinGw;;
+    *)          MACHINE="UNKNOWN:${unameOut}"
+esac
+
+echo "Install Slack..."
+brew install --cask slack
