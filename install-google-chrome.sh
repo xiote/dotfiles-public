eb60e74130727a5424c3216e5001fc0398b781e4
xioteOper <xioteOper@sac7.local>
2024-07-08 09:43:43 +0900

    new file:   install-google-chrome.sh

---
+++ install-google-chrome.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     MACHINE=Linux;;
+    Darwin*)    MACHINE=Mac;;
+    CYGWIN*)    MACHINE=Cygwin;;
+    MINGW*)     MACHINE=MinGw;;
+    *)          MACHINE="UNKNOWN:${unameOut}"
+esac
+
+echo "Install GoogleChrome..."
+brew install --cask google-chrome
